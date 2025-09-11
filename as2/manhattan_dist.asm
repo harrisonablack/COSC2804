64046c02d1b57476b5674c0a02ebf56e8b6d2cbd
Harrison Black <harrison.black18@Gmail.com>
2025-09-11 13:16:06 +1000

    Initial Files for Assesment 2

---
+++ as2/manhattan_dist.asm
@@ -0,0 +1,9 @@
+.ORIG x3000
+
+; your code here
+
+HALT
+; your code here
+INBOUNDS .STRINGZ "The player is within distance of the goal"
+OUTBOUNDS .STRINGZ "The player is outside the goal bounds"
+.END
